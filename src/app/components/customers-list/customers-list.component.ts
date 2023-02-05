0b654f3ecd925ea4fc66d26fa01571c1814dde75
AlexanderBykanov <aleksandrbykanov1986@gmail.com>
2023-02-05 13:25:55 +0200

    Сделал reset & save

--- src/app/components/customers-list/customers-list.component.ts
+++ src/app/components/customers-list/customers-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CustomerInterface } from 'src/app/shared/data/types/customer.interface';
 import { HttpService } from 'src/app/shared/services/http.service';
 
 
@@ -9,10 +10,49 @@ import { HttpService } from 'src/app/shared/services/http.service';
 })
 export class CustomersListComponent implements OnInit{
 
+  isEditPos!: number | null;
+  isNotChanged!: boolean;
+  private tempCustomer!: CustomerInterface;
+
   constructor(public httpService: HttpService) {}
 
   ngOnInit() {
-    this.httpService.getData()
+    this.httpService.getData();
+    this.resetEditStatus();
   }
+
+  editCustomer(i: number): void{
+    this.resetEditStatus();
+    this.isEditPos = i;
+  }
+
+  setValue(key: string, original: string, value: string): void {
+    const valueTrim = value.trim();
+
+    if(original === valueTrim) return;
+    if(original === this.tempCustomer[key as keyof CustomerInterface]) return;
+
+    this.tempCustomer[key as keyof CustomerInterface] = valueTrim;
+    this.isNotChanged = false;
+  }
+
+  cancelEdit(): void{
+    this.resetEditStatus();
+  }
+
+  saveCustomer(): void{
+    console.log(this.tempCustomer);
+    this.resetEditStatus();
+  }
+
+  deleteCustomer(): void{}
+
+  private resetEditStatus() {
+    this.isEditPos = null;
+    this.isNotChanged = true;
+    this.tempCustomer = this.resetCustomer();
+  }
+
+  private resetCustomer = (): CustomerInterface => ({name: '', email: '', mobile: '', location: ''});
 
 }
